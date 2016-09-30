e6a8e707cddc9fec93e8f3fb08b7aecccd651afd
Carlos Andres Charris S <darkklitos@gmail.com>
2016-09-30 17:15:14 -0500

    Configuracion del Task Runner y Servidor local con livereload

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+//File Gulpfile.js
+'use strict'
+
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    historyApiFallback = require('connect-history-api-fallback');
+
+// Servidor web de desarrollo
+gulp.task('server', function() {
+    connect.server({
+        root: './app',
+        hostname: '0.0.0.0',
+        port: 9090,
+        livereload: true,
+        middleware: function(connect, opt) {
+            return [historyApiFallback()];
+        }
+    });
+});
